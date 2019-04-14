73e0958bd92ee450579150c802331933002c8c1b
Mel <mwilsonreyes@gmail.com>
2019-04-14 15:08:57 -0500

    Added dynamic way to create LSTM models.

--- model_pipeline.py
+++ model_pipeline.py
@@ -6,20 +6,57 @@ from keras.layers import Dense, Dropout, LSTM
 from keras.utils import np_utils
 import re
 
-# TODO: make dynamic
-# Currently Adapted from:
+# Constructs a LSTM Model based on hyperparamters
+# Parameters:
+#   @input_shape
+#   @output_shape
+#   @layers is the number of LSTM layers that will be added to the model.
+#   @units is the number of units per layer stored in a list.
+#   @dropout is the dropout value per layer stored in a list.
+#   @activation is the activation function for the last dense layer.
+#   @optimizer is the optimizer to use when compiling the model.
+#   @verbose adds print statements to track progress of model.
+#
+# Return:
+#   @model
+# Adapted from:
 #   https://www.analyticsvidhya.com/blog/2018/03/text-generation-using-python-nlp/
-def construct_model(X_set, Y_set, verbose = True):
+def construct_lstm_model(input_shape, output_shape, layers = 1, units = [400], dropout = [0.25],
+    activation = 'softmax', optimizer = "adam", verbose = True):
+
     # Make model
-    print("Making model...")
+    if verbose:
+        print("Making model...")
+
+    if (layers < 1):
+        print("ERROR construct_lstm_model: must have at least one layer.")
+        return
+
+    if (layers != len(units) or layers != len(dropout)):
+        print("ERROR construct_lstm_model: units per layer are unspecified.")
+        return
 
     model = Sequential()
-    model.add(LSTM(400, input_shape = (X_set.shape[1], X_set.shape[2]), return_sequences = True))
-    model.add(Dropout(0.25))
-    model.add(LSTM(400))
-    model.add(Dropout(0.25))
-    model.add(Dense(Y_set.shape[1], activation = 'softmax'))
-    model.compile(loss = 'categorical_crossentropy', optimizer = 'adam')
+
+    # Add first layer
+    model.add(LSTM(units[0], input_shape = input_shape, return_sequences = True))
+
+    if (len(dropout) > 1):
+        model.add(Dropout(dropout[0]))
+
+    # Add remaining layers if specified
+    for index in np.arange(1, layers):
+        model.add(LSTM(units[index]))
+        model.add(Dropout(dropout[index]))
+
+    # Add the final layer to make prediction
+    model.add(Dense(output_shape, activation = activation))
+
+    # Add loss and optimizer
+    model.compile(loss = 'categorical_crossentropy', optimizer = optimizer)
+
+    if verbose:
+        print(model.summary())
 
     return model
 
@@ -41,6 +78,9 @@ def generate_prediction(model, X, seq_length):
     print([n_to_char[value] for value in X])
     print([n_to_char[value] for value in string_mapped])
 
+def log():
+    return
+
 if __name__ == "__main__":
 
     X_set = np.load("data/sequences.npy")
@@ -53,13 +93,16 @@ if __name__ == "__main__":
     char_to_n = {char:n for n, char in enumerate(characters)}
 
     # Make model
-    model = construct_model(X_set, Y_set)
+    model = construct_lstm_model((X_set.shape[1], X_set.shape[2]),
+        Y_set.shape[1], layers = 2, units = [200, 200], dropout = [0.25, 0.25])
 
     # Model training
-    model.fit(X_set, Y_set, epochs = 10)
+    model.fit(X_set, Y_set, epochs = 1)
 
     X = np.array([50, 68, 65, 1, 62, 78, 61, 74, 64, 1, 74, 65, 83, 1, 73, 61,
     74, 81, 79]).tolist()
 
     # Generate prediction
     generate_prediction(model, X, 19)
+
+    # Log result
